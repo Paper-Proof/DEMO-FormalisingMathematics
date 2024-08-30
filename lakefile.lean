97adb420da38291a3b20b5d40aa3a67148c69367
Evgenia Karunus <lakesare@gmail.com>
2024-08-30 17:09:24 +0500

    `devcontainer.json` - add paperproof

--- lakefile.lean
+++ lakefile.lean
@@ -12,6 +12,9 @@ package «formalising-mathematics-2024» where
 require mathlib from git
   "https://github.com/leanprover-community/mathlib4.git"
 
+require Paperproof from git
+  "https://github.com/Paper-Proof/paperproof.git"@"main"/"lean"
+
 @[default_target]
 lean_lib «FormalisingMathematics2024» {
   -- add any library configuration options here
